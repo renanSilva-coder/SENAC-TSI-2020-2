c66f2fa8ae7572d677893ddd000a6d7605738cc2
Renan Augusto <renan.augusto09@gmail.com>
2020-08-20 22:45:15 -0300

    Aula 20.08

--- operadores/index.php
+++ operadores/index.php
@@ -17,7 +17,7 @@ echo '<br><br><br>Seu saldo é .$saldo';
 //operador de subtração
 $saldo = $saldo - 10; //ou $saldo -= 10
 //operador de multiplicação
-$saldo *= 1000 ;
+$saldo *= 14684 ;
 
 
 echo "<br><br><br>Agora seu saldo é .$saldo";
