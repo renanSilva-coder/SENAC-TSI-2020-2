eab43c752064ecbf22628088759394dab3e89e1d
Renan Augusto <renan.augusto09@gmail.com>
2020-08-20 22:20:09 -0300

    Aula 20-08

---
+++ operadores/index.php
@@ -0,0 +1,23 @@
+<?php
+echo "Olá Mundo!";
+
+//comentário de linha
+/**
+ * comentário de bloco
+ *  
+*/
+
+$saldo = 34.5;
+//Aspas duplas aceitam string especial (Pode ter variável)
+echo "<br><br><br>Seu saldo é .$saldo";
+
+//Aspas simples apenas string pura (Mostra tudo como string)
+echo '<br><br><br>Seu saldo é .$saldo';
+
+//operador de subtração
+$saldo = $saldo - 10; //ou $saldo -= 10
+//operador de multiplicação
+$saldo *= 1000 ;
+
+
+echo "<br><br><br>Agora seu saldo é .$saldo";
