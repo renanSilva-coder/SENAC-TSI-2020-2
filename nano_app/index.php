abfb28bcd6ae3ed45cd01c203d216a9ac0c72a4b
Renan Augusto <renan.augusto09@gmail.com>
2020-10-15 19:25:52 -0300

    passando o nano_app

---
+++ nano_app/index.php
@@ -0,0 +1,9 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors',1);
+error_reporting(E_ALL);
+
+// myseli_connect('host','user','password','nameDb');
+$db = mysqli_connect('localhost','root','','php'); //Conecta o SGBD;
+
